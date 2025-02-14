568097eba896c2c5274dab34139109db91fe02f6
Amitkr789 <90599465+Amitkr789@users.noreply.github.com>
2025-02-15 00:56:35 +0530

    Update settings.py

--- LeadSoft/settings.py
+++ LeadSoft/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["finalui.onrender.com", "localhost", "127.0.0.1"]
 # ALLOWED_HOSTS = ["leadsoft2-1.onrender.com", "localhost", "127.0.0.1"]
 
 
